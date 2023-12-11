3ec359d00b26ad4352fb0192e0c60edff12b3258
sirlyun <sirlyun23@gmail.com>
2023-12-11 19:50:02 +0900

    first setting

---
+++ src/test/java/dasanda/BE/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package dasanda.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
